95e3668cbd6872d8f4d0ae9780c25828b665c0bb
Stanislava Pavlova <stanislavap2002@gmail.com>
2022-11-27 19:38:00 +0200

    added fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
-import "../scss/app.scss";
-
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   // This block will be executed once the page is loaded and ready
+  const price = document.querySelectorAll('.price');
+
+  for (let p = 0; p < price.length; p++) {
+    if (price[p].classList.contains('hot')) {
+      price[p].textContent += ' 🔥';
+    }
+  }
 });
